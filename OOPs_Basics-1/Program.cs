2442115d82d27f90cac8e9169ea37c574ae1a3a9
SaiDurga <saiprasadveluri@gmail.com>
2025-08-09 07:11:52 +0000

    committ

--- OOPs_Basics-1/Program.cs
+++ OOPs_Basics-1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -47,40 +48,195 @@ using System.Threading.Tasks;
 //}
 
 /// 2...Polymorphism, MethodOverloading ///
+///// 
+//namespace OOPs_Basics_1
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Payment baaay = new Creditpayment();
+//            baaay.pay(20000);
+//            baaay = new Creditpayment();
+//            baaay.pay(5000);
+
+//        }
+//    }
+
+
+//    class Payment
+//    {
+//        public virtual void pay(double amount)
+//        {
+//            Console.WriteLine($"the double amount is {amount}");
+//        }
+//    }
+//    class Creditpayment : Payment
+//    {
+//        public override void pay(double amount)
+//        {
+//            Console.WriteLine($"The creditcard payment is {amount}");
+//        }
+//    }
+//    class UPIPayment : Payment
+//    {
+//        public override void pay(double amount)
+//        {
+//            Console.WriteLine($"the amount of Upi spend is {amount}");
+//        }
+//    }
+//}
+
+
+/// 3... Abstraction /////
+/// 
+//namespace OOPs_Basics_1
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            report rp = new Salesreport();
+//            rp.printheader();
+//            rp.generate();
+//        }
+//    }
+
+//   abstract class report
+//    {
+//        public abstract void generate(); // no boddy , must be implemented ;
+
+//        public void printheader()
+//        {
+//            Console.WriteLine("=====print header =====");
+//        }
+//    }
+//    class Salesreport : report
+//    {
+//        public override void generate()
+//        {
+//            Console.WriteLine("slaes repprt is generating : ");
+//        }
+//    }
+//}
+
+///// 4... Encapsulation ////
+
+//namespace OOPs_Basics_1
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            bankAccount acc = new bankAccount();
+//            acc.Balance = 500;
+//            Console.WriteLine(acc.Balance);
+//            acc.deposit(400);
+//        }
+//    }
+
+//    class bankAccount
+//    {
+//        private double balance;
+//        public double Balance
+//        {
+//            get { return balance; }
+
+//            set
+//            {
+//                if (value >= 0)
+//                {
+//                    balance = value;
+//                }
+//                else
+//                {
+//                    Console.WriteLine("balance is in negative state :....");
+//                }
+//            }
+
+//        }
+//            public void deposit(double amount)
+//        {
+//            Balance += amount;
+//            Console.WriteLine($"the balance amout is {Balance}");
+//        }
+//    }
+
+
+
+//}
+
+
+// 5... Interfaces ...////
+
+//namespace OOPs_Basics_1
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Multiparent ma = new Multiparent();
+//            ma.print();
+//            ma.scann();
+//        }
+//    }
+
+//    interface Iprinter
+//    {
+//         void print();
+//    }
+
+//    interface Iscanner
+//    {
+//         void scann();
+//    }
+//    class Multiparent : Iprinter, Iscanner
+//    {
+//        public void print()
+//        {
+//            Console.WriteLine("the printer is printing....");
+//        }
+//        public void scann()
+//        {
+//            Console.WriteLine("the scanner is scanning...");
+//        }
+//    }
+
+//}
+
+
+/// 6...Constructor Overloading ... ///
+/// 
 namespace OOPs_Basics_1
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Payment baaay = new Creditpayment();
-            baaay.pay(20000);
-            baaay = new Creditpayment();
-            baaay.pay(5000);
-
+            book bk = new book("c#");
+            book bkl = new book("faf", "zara");
+            bk.showinfo();
+            bkl.showinfo();
         }
     }
 
+    class book
+    {
+        public string Title;
+        public string Author;
 
-    class Payment
-    {
-        public virtual void pay(double amount)
+        public  book(string title)
         {
-            Console.WriteLine($"the double amount is {amount}");
+            Title = title;
+        Author = "unknow";
         }
-    }
-    class Creditpayment : Payment
-    {
-        public override void pay(double amount)
-        {
-            Console.WriteLine($"The creditcard payment is {amount}");
+        public book(string title, string author) {
+            Title = title;
+            Author = author;
         }
-    }
-    class UPIPayment : Payment
-    {
-        public override void pay(double amount)
+        public void showinfo()
         {
-            Console.WriteLine($"the amount of Upi spend is {amount}");
+            Console.WriteLine($"bookname is {Title},and the auhtor name is {Author}");
         }
     }
 }
