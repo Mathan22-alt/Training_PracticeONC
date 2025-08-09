c2033f440e805c9bbd945f44ef6bb16cce192da4
SaiDurga <saiprasadveluri@gmail.com>
2025-08-09 05:32:23 +0000

    mm

--- OOPs_Basics-1/Program.cs
+++ OOPs_Basics-1/Program.cs
@@ -8,40 +8,79 @@ using System.Threading.Tasks;
 
 ///  1..Inheritance  //
 
+//namespace OOPs_Basics_1
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Manager mng = new Manager
+//            {
+//                salary = 5000,
+//                name ="sairam",
+//                department= "finance"
+//            };
+//            mng.Display();
+//            mng.LeaveApproval();
+//        }
+//    }
+
+//    class Employeee
+//    {
+//        public int salary { get; set; }
+//        public string name { get; set; }
+
+//        public void Display()
+//        {
+//            Console.WriteLine($"name of the employee is {name},salary of the employee is {salary}");
+//        }
+//    }
+//    class Manager : Employeee
+//    {
+//        public string department { get; set; }
+
+//        public void LeaveApproval()
+//        {
+//            Console.WriteLine($"Leave approved for the {name},from the {department}");
+//        }
+//    }
+//}
+
+/// 2...Polymorphism, MethodOverloading ///
 namespace OOPs_Basics_1
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Manager mng = new Manager
-            {
-                salary = 5000,
-                name ="sairam",
-                department= "finance"
-            };
-            mng.Display();
-            mng.LeaveApproval();
+            Payment baaay = new Creditpayment();
+            baaay.pay(20000);
+            baaay = new Creditpayment();
+            baaay.pay(5000);
+
         }
     }
 
-    class Employeee
-    {
-        public int salary { get; set; }
-        public string name { get; set; }
 
-        public void Display()
+    class Payment
+    {
+        public virtual void pay(double amount)
         {
-            Console.WriteLine($"name of the employee is {name},salary of the employee is {salary}");
+            Console.WriteLine($"the double amount is {amount}");
         }
     }
-    class Manager : Employeee
+    class Creditpayment : Payment
     {
-        public string department { get; set; }
-
-        public void LeaveApproval()
+        public override void pay(double amount)
         {
-            Console.WriteLine($"Leave approved for the {name},from the {department}");
+            Console.WriteLine($"The creditcard payment is {amount}");
+        }
+    }
+    class UPIPayment : Payment
+    {
+        public override void pay(double amount)
+        {
+            Console.WriteLine($"the amount of Upi spend is {amount}");
         }
     }
 }
